a36d04d55282238b987bf4976e4ed338ca399d19
Joshua Singleton <jsingle345@gmail.com>
2020-09-14 02:31:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,6 +129,7 @@ end
 
 
 
-def num_points_scored(player_name)
-  game_hash
+def num_points_scored player_name
+  players = game_hash[:home][:players].merge(game_hash[:away][:players])
+  players[player_name][:points]
 end
